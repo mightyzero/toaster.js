7d60a06d149625a8c5c84fc2ffb130b7c823506e
Minh Pham <minh.pham@utexas.edu>
2015-10-11 08:46:30 -0500

    Added linting task to gulfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp');
 
+// Include plugins
+var jshint = require('gulp-jshint');
 var nodemon = require('gulp-nodemon');
+
+// Lint JavaScript code with jsthint
+gulp.task('lint', function() {
+	return gulp
+			.src('app/**/*.js')
+			.pipe(jshint())
+			.pipe(jshint.reporter('default'));
+});
 
 gulp.task('default', function() {
 	nodemon({
 		script: 'server.js',
 		ext: 'js',
 		ignore: ['./node_modules/**']
-	})
+	});
 });
